f1dbd44450fbe032c44201e2167f61ba1fe4f4c9
Allen Zhao <cz264@njit.edu>
2017-02-13 16:53:55 -0500

    almost finished

--- app/src/main/java/com/example/cz264/keepitup/MainActivity.java
+++ app/src/main/java/com/example/cz264/keepitup/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.cz264.keepitup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
+    public static final String EXERCISE_WEIGHTS = "Weight lifting";
+    public static final String EXERCISE_YOGA = "Yoga";
+    public static final String EXERCISE_CARDIO = "Cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        RelativeLayout weigtBtn = (RelativeLayout) findViewById(R.id.weightBtn);
+        RelativeLayout yogaBtn = (RelativeLayout) findViewById(R.id.yogaBtn);
+        RelativeLayout cardioBtn = (RelativeLayout) findViewById(R.id.cardioBtn);
+
+        weigtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(MainActivity.EXERCISE_WEIGHTS);
+            }
+        });
+
+        yogaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(MainActivity.EXERCISE_YOGA);
+            }
+        });
+
+        cardioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(MainActivity.EXERCISE_CARDIO);
+            }
+        });
     }
+
+    private void loadDetailActivity(String exerciseTitle) {
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, exerciseTitle);
+        startActivity(intent);
+    }
+
 }
